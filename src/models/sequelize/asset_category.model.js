dd37fd154789f9a04a5e97b38cb6a9f0d986ec57
Colman Maximo <colmanmaximo2003@gmail.com>
2025-09-29 11:26:07 -0300

    Se realizó la relacion de muchos a muchos en Sequelize

--- src/models/sequelize/asset_category.model.js
+++ src/models/sequelize/asset_category.model.js
@@ -1,7 +1,37 @@
 import { DataTypes } from "sequelize";
+import { AssetModel } from "./asset.model.js";
+import { CategoryModel } from "../mongoose/category.model.js";
 
 export const AssetCategoryModel = sequelize.define("AssetCategory", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+});
+
+//Relación muchos a muchos
+
+AssetModel.belongsToMany(CategoryModel, {
+  through: ArticleTag,
+  foreignKey: "asset_id",
+  as: "asset",
+  onDelete: "CASCADE"
+});
+
+CategoryModel.belongsToMany(AssetModel, {
+  through: ArticleTag,
+  foreignKey: "category_id",
+  as: "category",
+  onDelete: "CASCADE  ",
+});
+
+CategoryModel.belongsTo(asset, {
+  foreignKey: "asset_id",
+  as: "asset",
+  onDelete: "CASCADE",
+});
+
+CategoryModel.belongsTo(articles, {
+  foreignKey: "category_id",
+  as: "category",
+  onDelete: "CASCADE"
 });
 
 // TODO: completar relaciones muchos a muchos entre Asset y Category mediante AssetCategory.
